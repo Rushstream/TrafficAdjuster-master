d0d882fc85ccc45eff0b54b8cfe27baa6529c086
Rushstream <rusherdeouf@gmail.com>
2019-07-05 21:10:26 +0200

    Add files via upload
    - Modifie le trafic sur les routes, il passe de 100% à 0%. Les valeurs supérieures à 100% sont également prises en charge mais non recommandées. Une valeur inférieure à 0% entraînera un arrêt brutal du jeu.
    - Change le nombre de Peds dans les rues
    - Change le nombre de voitures garées dans le monde
    - Une bascule entre true et false, ceci active / désactive diverses fonctionnalités telles que:
    - Réponses de la police
    - Réponses du service d'incendie
    - Réponses Swat
    - Réponses des ambulances
    - barrages routiers
    - Des gangs

---
+++ config.lua
@@ -0,0 +1,19 @@
+TrafficAmount = 100 
+PedestrianAmount = 100
+ParkedAmount = 100
+EnableDispatch = true
+
+
+--[[
+- Modifie le trafic sur les routes, il passe de 100% à 0%. Les valeurs supérieures à 100% sont également prises en charge mais non recommandées. Une valeur inférieure à 0% entraînera un arrêt brutal du jeu.
+- Change le nombre de Peds dans les rues
+- Change le nombre de voitures garées dans le monde
+- Une bascule entre true et false, ceci active / désactive diverses fonctionnalités telles que:
+- Réponses de la police
+- Réponses du service d'incendie
+- Réponses Swat
+- Réponses des ambulances
+- barrages routiers
+- Des gangs
+
+]]
